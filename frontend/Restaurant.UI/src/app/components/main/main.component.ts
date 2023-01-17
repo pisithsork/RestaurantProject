a6762d97144e365664fa77a19a7eeb39335b1c1e
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-01-17 16:30:43 -0500

    Able to show restaurant info on main

--- frontend/Restaurant.UI/src/app/components/main/main.component.ts
+++ frontend/Restaurant.UI/src/app/components/main/main.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from '../../models/restaurant';
+import { RestaurantService } from '../../services/restaurant.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+
+  AllRestaurant: Restaurant[] = [];
+
+  constructor(private restService: RestaurantService){}
+
+  ngOnInit(): void{
+    this.restService.getAllRestaurant().subscribe(
+        (resp) => this.AllRestaurant = resp,
+        (error) => console.log(error)
+    );
+  }
 
 }
---
+++ frontend/Restaurant.UI/src/app/services/restaurant.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Restaurant } from '../models/restaurant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestaurantService {
+
+  baseUrl: string = "https://localhost:7097";
+  constructor(private httpClient: HttpClient) { }
+
+  getAllRestaurant(): Observable<any>{
+    return this.httpClient.get<any>(`${this.baseUrl}/api/Restaurants`)
+  }
+}
