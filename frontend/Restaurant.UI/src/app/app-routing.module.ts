dcf002817338988f934d0e075cb5010fb7c1a513
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-01-17 16:28:34 -0500

    Created service to get all restaurants

--- frontend/Restaurant.UI/src/app/app-routing.module.ts
+++ frontend/Restaurant.UI/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
   {path: "home", component: MainComponent}
 ];
 
