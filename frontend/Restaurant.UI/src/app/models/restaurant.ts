7bda648289f3dd06943d41721d8c4443ebaf48c1
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-01-17 18:16:24 -0500

    Updated Restaurant model to have score, menu, grade

--- frontend/Restaurant.UI/src/app/models/restaurant.ts
+++ frontend/Restaurant.UI/src/app/models/restaurant.ts
@@ -1,4 +1,6 @@
 import { Menu } from "./menu";
+import { Score } from "./score";
+import { Grade } from "./grade";
 export class Restaurant{
     id: number;
     rest_id: number;
@@ -7,9 +9,11 @@ export class Restaurant{
     city: string;
     state: string;
     cuisine_type: string;
-    menu: Menu;
+    menu: Menu[];
+    score: Score[];
+    grade: Grade[];
 
-    constructor (id: number, rest_id: number, name: string, address: string, city: string, state: string, cuisine_type: string, menu: Menu){
+    constructor (id: number, rest_id: number, name: string, address: string, city: string, state: string, cuisine_type: string, menu: Menu[], score: Score[], grade: Grade[]){
         this.id = id;
         this.rest_id = rest_id;
         this.name = name;
@@ -18,5 +22,7 @@ export class Restaurant{
         this.state = state;
         this.cuisine_type = cuisine_type;
         this.menu = menu;
+        this.score = score;
+        this.grade = grade;
     }
 }
