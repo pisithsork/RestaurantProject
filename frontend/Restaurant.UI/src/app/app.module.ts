954a71d4be16760ecd5bc7a3f3ed7ffb194a70ec
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-01-19 15:49:44 -0500

    Forgot to get rid of deleted displaymenu component in app.module.ts whoops

--- frontend/Restaurant.UI/src/app/app.module.ts
+++ frontend/Restaurant.UI/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DisplayMenuComponent } from './components/display-menu/display-menu.component';
 import { RestaurantDetailsComponent } from './components/restaurant-details/restaurant-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    DisplayMenuComponent,
     RestaurantDetailsComponent
   ],
   imports: [
